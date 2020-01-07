7a6560a0e91b9a59ecfc3364115a65fb68524556
42yeah <potion@live.cn>
2020-01-07 12:52:56 +0800

    Initial Commit

---
+++ IPFinal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/7.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
