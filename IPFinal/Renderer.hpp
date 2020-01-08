8f471900819908e4340ae760a63830d08f5fcc2d
42yeah <potion@live.cn>
2020-01-08 13:55:53 +0800

    Added GLAD

---
+++ IPFinal/Renderer.hpp
@@ -0,0 +1,18 @@
+//
+//  Renderer.hpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/8.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#ifndef Renderer_hpp
+#define Renderer_hpp
+
+
+class Renderer {
+public:
+    Renderer() {}
+};
+
+#endif /* Renderer_hpp */
