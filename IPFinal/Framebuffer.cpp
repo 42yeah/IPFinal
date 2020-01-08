72005b01380147de0063ed38227ecb5b7f242691
42yeah <potion@live.cn>
2020-01-08 21:06:54 +0800

    Added framebuffer

---
+++ IPFinal/Framebuffer.cpp
@@ -0,0 +1,19 @@
+//
+//  FrameBuffer.cpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/8.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#include "Framebuffer.hpp"
+
+
+Framebuffer::Framebuffer() {
+    glGenFramebuffers(1, &FBO);
+}
+
+void Framebuffer::bind() {
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+}
+
---
+++ IPFinal/Framebuffer.hpp
@@ -0,0 +1,25 @@
+//
+//  FrameBuffer.hpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/8.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#ifndef Framebuffer_hpp
+#define Framebuffer_hpp
+
+#include "glad/glad/glad.h"
+
+
+class Framebuffer {
+public:
+    Framebuffer();
+    
+    void bind();
+    
+private:
+    GLuint FBO;
+};
+
+#endif /* Framebuffer_hpp */
