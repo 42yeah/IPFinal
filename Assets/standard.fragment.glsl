2b68280296e2d9e2f47f4938215f2c5e2dcd3178
42yeah <potion@live.cn>
2020-01-08 17:43:58 +0800

    Abstracted into function

--- Assets/standard.fragment.glsl
+++ Assets/standard.fragment.glsl
@@ -8,13 +8,16 @@ out vec4 color;
 uniform samplerBuffer cameraTexture;
 uniform vec2 resolution;
 
-void main() {
-    vec2 invUV = vec2(uv.x, 1.0 - uv.y);
+
+vec4 textureCam(vec2 _uv) {
+    vec2 invUV = vec2(1.0 - _uv.x, 1.0 - _uv.y);
     vec2 sp = invUV * resolution;
     ivec2 samplePos = ivec2(floor(sp.x), floor(sp.y));
     int offset = (samplePos.x + int(samplePos.y * resolution.x)) * 3;
-    vec3 sampled = vec3(texelFetch(cameraTexture, offset + 2).r,
-        texelFetch(cameraTexture, offset + 1).r,
-        texelFetch(cameraTexture, offset).r);
-    color = vec4(sampled.r, sampled.g, sampled.b, 1.0);
+    vec3 sampled = vec3(texelFetch(cameraTexture, offset + 2).r, texelFetch(cameraTexture, offset + 1).r, texelFetch(cameraTexture, offset).r); // BGR
+    return vec4(sampled, 1.0);
+}
+
+void main() {
+    color = vec4(textureCam(uv).rgb, 1.0);
 }
