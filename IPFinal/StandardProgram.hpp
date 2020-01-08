52c461e3e612e4c980c49e89fe2df0205b5317c8
42yeah <potion@live.cn>
2020-01-08 14:08:14 +0800

    Ripped off StandardProgram from 3DSnooker. Well shouldn't say rip off. They are all mine anyways.

---
+++ IPFinal/StandardProgram.hpp
@@ -0,0 +1,40 @@
+//
+//  StandardProgram.hpp
+//  Snooker3D
+//
+//  Created by apple on 2019/11/23.
+//  Copyright © 2019 aiofwa. All rights reserved.
+//
+
+#ifndef StandardProgram_hpp
+#define StandardProgram_hpp
+
+#include "glad/glad/glad.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+/**
+ Explanation about why it being "standard":
+ Standard only applies to all Snooker3D shaders, which means it has all uniforms we need,
+ And the data are at the right place.
+ */
+class StandardProgram {
+public:
+    StandardProgram();
+
+    void link(std::string vertexShaderPath, std::string fragmentShaderPath);
+    GLuint compile(GLuint shaderType, std::string shaderPath);
+    void use();
+    void configVertexPointers();
+
+private:
+    GLuint program;
+    
+    // === LOCATIONS === //
+    GLuint aPosLoc;
+};
+
+#endif /* StandardProgram_hpp */
