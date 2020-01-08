345c90476de69935e3ca6a351e1fc7d320a38dbc
42yeah <potion@live.cn>
2020-01-08 13:42:51 +0800

    Introduced GLFW

---
+++ IPFinal/Window.cpp
@@ -0,0 +1,9 @@
+//
+//  Window.cpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/8.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#include "Window.hpp"
---
+++ IPFinal/Window.hpp
@@ -0,0 +1,23 @@
+//
+//  Window.hpp
+//  IPFinal
+//
+//  Created by apple on 2020/1/8.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#ifndef Window_hpp
+#define Window_hpp
+
+#include <GLFW/glfw3.h>
+
+
+class Window {
+public:
+    Window();
+    
+private:
+    GLFWwindow *window;
+};
+
+#endif /* Window_hpp */
