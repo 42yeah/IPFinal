b524637ef82d069a8038a1f1093e0a21cb94906d
42yeah <potion@live.cn>
2020-01-09 15:28:41 +0800

    Introduced tone mapping

--- Assets/hdr.fragment.glsl
+++ Assets/hdr.fragment.glsl
@@ -10,5 +10,9 @@ uniform sampler2D hdrTexture;
 
 
 void main() {
-    color = vec4(texture(hdrTexture, uv).rgb, 1.0);
+    const float gamma = 2.2;
+    vec3 hdrColor = texture(hdrTexture, uv).rgb;
+    vec3 mapped = hdrColor / (hdrColor + vec3(1.0));
+    mapped = pow(mapped, vec3(1.0 / gamma));
+    color = vec4(mapped, 1.0);
 }
